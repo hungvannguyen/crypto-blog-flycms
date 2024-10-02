168f434fe1c86cccfcada0bfdf036958e646d48b
hungvannguyen <nguyenvanhung08012002@gmail.com>
2024-10-02 09:28:16 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,21 @@ export function formatDate(dateString) {
     const year = date.getFullYear();
     return `${day} ${month} ${year}`;
 }
+
+
+export async function fetchPosts(endpoint, page = 1, limit = 9, ajax = 1) {
+    try {
+        const url = window.location.origin + endpoint + (endpoint.includes('?') ? '&' : '?') + 'page=' + page + '&limit=' + limit + '&ajax=' + ajax;
+        console.log(url);
+        const response = await fetch(url);
+        const data = await response.text();
+        const dataStr = data.replaceAll('\n', '').replaceAll('&quot;', '"');
+        const jsonData = JSON.parse(dataStr);
+        console.log(jsonData);
+        return jsonData;
+    }
+    catch (error) {
+        console.error('Error fetching data:', error);
+        return { data: [] };
+    }
+}
