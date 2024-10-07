5095c81b1f4bf00e3d1ab515d3a53d3ff9dc4936
hungvannguyen <nguyenvanhung08012002@gmail.com>
2024-10-07 16:12:21 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
+var toTopButton = document.getElementById("to-top-button");
+
+if (toTopButton) {
+
+    window.onscroll = function() {
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+            toTopButton.classList.remove("hidden");
+        } else {
+            toTopButton.classList.add("hidden");
+        }
+    };
+
+    window.goToTop = function() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+}
+
 export function formatDate(dateString) {
     const date = new Date(dateString);
     const day = date.getDate();
